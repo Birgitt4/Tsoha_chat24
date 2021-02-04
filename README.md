29897b0ba3436f9f7322692f886776c9bb605725
Birgitta <birgitta.kaplas@kolumbus.fi>
2021-02-04 15:03:30 +0200

    editing message works

--- README.md
+++
@@ -1,5 +0,0 @@
-# Tietokantasovellus
-
-Tämä repositorio on luotu Helsingin yliopiston Tietokantasovellus kurssia varten.
-
-Toteutan kurssilla yhden valmiiksi annetuista aiheista, keskustelusovelluksen.
--- schema.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT, status TEXT);
-CREATE TABLE threads (id SERIAL PRIMARY KEY, title TEXT, content TEXT, user_id INTEGER REFERENCES users);
-CREATE TABLE messages (id SERIAL PRIMARY KEY, content TEXT, thread_id INTEGER REFERENCES threads, user_id INTEGER REFERENCES users);
-CREATE TABLE friends (id SERIAL PRIMARY KEY, user_id INTEGER REFERENCES users, friend_id INTEGER REFERENCES users);
