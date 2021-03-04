fc3a099e9cdc8326da44a4be27ec7ec6ff698183
Birgitta <birgitta.kaplas@kolumbus.fi>
2021-03-04 20:49:13 +0200

    safety checks (and cleaning)

--- routes.py
+++ routes.py
@@ -9,8 +9,8 @@ def home():
     list = threads.get_threads()
     return render_template("home.html", threads=list)
 
-@app.route("/thread/<int:id>", methods=["get", "post"])
-def thread(id):
+@app.route("/thread/<int:id>/<int:m_id>/<int:e>")
+def thread(id, m_id, e):
     allow = False
     list = messages.get_messages(id)
     starter = threads.get_thread(id)
@@ -20,16 +20,44 @@ def thread(id):
                 allow = True
     elif starter[0][4] == 0:
         allow = True
-    if request.method == "POST":
-        content = request.form["content"]
-        if (content != ""):
-            messages.add_message(content, id)
     if users.logged() == True:
         is_admin = users.is_admin()
     else:
         is_admin = False
     return render_template("thread.html", starter=starter, messages=list, id=id, 
-        is_admin=is_admin, message_id=0, allow=allow)
+        is_admin=is_admin, message_id=m_id, allow=allow, error=error(e))
+
+@app.route("/thread/<int:id>")
+def to_thread(id):
+    return redirect("/thread/"+str(id)+"/0/0")
+
+def error(e):
+    error = ""
+    if e == 1:
+        error = "Viesti on liian pitkä. Viestin pituus on max 500 merkkiä"
+    elif e == 2:
+        error = "Viesti ei voi olla tyhjä"
+    elif e == 3:
+        error = "Kirjaudu sisään lähettääksesi viestin"
+    return error
+
+@app.route("/thread/add/<int:id>", methods=["get", "post"])
+def new_message(id):
+    if users.session["csrf_token"] != request.form["csrf_token"]:
+        #abort
+        pass
+    e = 0
+    content = request.form["content"]
+    if not users.logged():
+        e = 3
+        print("false")
+    elif len(content) > 500:
+        e = 1
+    elif len(content) == 0:
+        e = 2
+    elif 0 < len(content) <=500:
+        messages.add_message(content, id)
+    return redirect("/thread/"+str(id)+"/0/"+str(e))
 
 @app.route("/thread/edit/<int:id>", methods=["get", "post"])
 def del_or_edit(id):
@@ -39,32 +67,47 @@ def del_or_edit(id):
         if option == "delete":
             messages.delete(id)
         elif option == "edit":
-            is_admin = users.is_admin()
-            list = messages.get_messages(thrd)
-            starter = threads.get_thread(thrd)
-            return render_template("thread.html", starter=starter, messages=list, 
-                id=thrd, is_admin=is_admin, message_id=id, allow=True)
+            return redirect("/thread/"+str(thrd)+"/"+str(id)+"/0")
     return redirect("/thread/"+str(thrd))
 
 @app.route("/thread/edit", methods=["get","post"])
 def edit():
+    if users.session["csrf_token"] != request.form["csrf_token"]:
+        #abort
+        pass
+    e = 0
     message_id = request.form["message_id"]
     thrd = request.form["thread_id"]
     content = request.form["content"]
-    messages.edit(message_id, content)
-    return redirect("/thread/"+str(thrd))
+    if len(content) > 500:
+        e = 1
+    elif len(content) == 0:
+        e = 2
+    else:
+        messages.edit(message_id, content)
+    return redirect("/thread/"+str(thrd)+"/0/"+str(e))
 
 @app.route("/thread/edit_starter/<int:id>", methods=["get", "post"])
 def starter_edit(id):
+    if users.session["csrf_token"] != request.form["csrf_token"]:
+        #abort
+        pass
     content = request.form["content"]
-    threads.edit_content(id, content)
-    return redirect("/thread/"+str(id))
+    e = 0
+    if len(content) > 500:
+        e = 1
+    elif len(content) == 0:
+        e = 2
+    else:
+        threads.edit_content(id, content)
+    return redirect("/thread/"+str(id)+"/0/"+str(e))
 
-@app.route("/save/<int:id>", methods=["get", "post"])
+@app.route("/save/<int:id>")
 def save(id):
     allow = False
     if users.logged():
-        if threads.is_private:
+        if threads.is_private(id):
+            print("on yksityinen")
             allow = False
         else:
             allow = True
@@ -72,14 +115,12 @@ def save(id):
             users.save(id)
     return redirect("/thread/"+str(id))
 
-@app.route("/friendlist/<int:id>", methods=["get", "post"])
+@app.route("/friendlist/<int:id>")
 def friends_to_thread(id):
     allow = False
     if threads.get_thread(id)[0][3] == users.user_id():
         allow = True
     if allow:
-        if request.method == "POST":
-            return redirect("/thread/"+str(id))
         friends = users.get_friends(id)
         starter = threads.get_thread(id)
         title = starter[0][0]
@@ -88,6 +129,9 @@ def friends_to_thread(id):
 
 @app.route("/add_friends/<int:id>", methods=["get", "post"])
 def add_f(id):
+    if users.session["csrf_token"] != request.form["csrf_token"]:
+        #abort
+        pass
     friend = request.form["friend"]
     friend_id = request.form[friend]
     threads.add_user(friend_id, id)
@@ -101,16 +145,37 @@ def new():
 
 @app.route("/send", methods=["get", "post"])
 def send_thread():
+    if users.session["csrf_token"] != request.form["csrf_token"]:
+        #abort
+        pass
     title = request.form["title"]
     content = request.form["content"]
     privat = request.form["private"]
+    message = ""
+    error = False
     thread_id = 0
-    if int(privat) == 0:
-        thread_id = threads.new_thread(title, content, 0)
-    if int(privat) == 1:
-        thread_id = threads.new_thread(title, content, 1)
-    if thread_id == 0:
-        return render_template("new_thread.html", error=True, title=title, content=content)
+    if len(content) > 500:
+        message = "Viesti saa olla max 500 merkkiä"
+        error = True
+    elif len(content) == 0:
+        message = "Viesti ei voi olla tyhjä"
+        error = True
+    elif len(title) > 100:
+        message = "Otsikko saa olla max 100 merkkiä"
+        error = True
+    elif len(title) == 0:
+        message = "Otsikko ei saa olla tyhjä"
+        error = True
+    if not error:
+        if int(privat) == 0:
+            thread_id = threads.new_thread(title, content, 0)
+        if int(privat) == 1:
+            thread_id = threads.new_thread(title, content, 1)
+        if thread_id == 0:
+            message = "Jokin meni vikaan, yritä uudelleen"
+            error = True
+    if error:
+        return render_template("new_thread.html", error=True, title=title, content=content, message=message)
     return redirect("/thread/"+str(thread_id))
 
 @app.route("/search", methods=["get", "post"])
@@ -137,6 +202,9 @@ def search():
 @app.route("/profile/<int:user_id>", methods=["get", "post"])
 def profile(user_id):
     if request.method == "POST":
+        if users.session["csrf_token"] != request.form["csrf_token"]:
+            #abort
+            pass
         users.add_friend(user_id)
     username = users.get_name(user_id)
     public_threads = users.get_threads(user_id, 0)
@@ -159,12 +227,18 @@ def my_profile():
 
 @app.route("/add_friend/<int:id>", methods=["get", "post"])
 def add_friend(id):
+    if users.session["csrf_token"] != request.form["csrf_token"]:
+        #abort
+        pass
     users.add_friend(id)
     my_profile()
     return redirect("/profile/"+str(users.user_id()))
 
 @app.route("/reject/<int:id>", methods=["get", "post"])
 def delete_friend(id):
+    if users.session["csrf_token"] != request.form["csrf_token"]:
+        #abort
+        pass
     users.delete_friend(id)
     my_profile()
     return redirect("/profile/"+str(users.user_id()))
@@ -193,13 +267,23 @@ def signup():
     if request.method == "GET":
         return render_template("signup.html", error=False)
     if request.method == "POST":
+        error = False
+        message = ""
         username = request.form["username"]
         password = request.form["password"]
-        if users.username_taken(username):
+        if len(username) > 30:
+            error = True
+            message = "Käyttäjänimi on liian pitkä (max 30 merkkiä)"
+        elif len(password) < 6:
+            error = True
+            message = "Salasanan on oltava vähintään 6 merkkiä"
+        elif users.username_taken(username):
+            error = True
             message = "Käyttäjänimi on jo käytössä"
-            return render_template("signup.html", error=True, message=message)
-        if password != request.form["passcheck"]:
+        elif password != request.form["passcheck"]:
+            error = True
             message = "Salasanat eivät täsmää"
+        if error:
             return render_template("signup.html", error=True, message=message)
         if users.signup(username, password):
             return redirect("/login")
--- users.py
+++ users.py
@@ -1,6 +1,7 @@
 from db import db
 from flask import session
 from werkzeug.security import check_password_hash, generate_password_hash
+import os
 
 def login(username, password):
     sql = "SELECT password, id FROM users WHERE username=:username"
@@ -12,6 +13,7 @@ def login(username, password):
         if check_password_hash(user[0], password):
             session["user_id"] = user[1]
             session["loggedin"] = True
+            session["csrf_token"] = os.urandom(16).hex()
             return True
         else:
             return False
@@ -19,6 +21,7 @@ def login(username, password):
 def logout():
     del session["user_id"]
     del session["loggedin"]
+    del session["csrf_token"]
 
 def signup(username, password):
     hash_val = generate_password_hash(password)
